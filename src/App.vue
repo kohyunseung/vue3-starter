4c75051fa5f032832c8eb5ce1b2f7a2d3e7e1336
hyunseung-ko <hs.ko@dongwha.com>
2022-12-21 17:03:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import AppBar from "@/components/layout/AppBar.vue";
+</script>
---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Logo from "@/assets/logo.svg";
+</script>
+<template>
+  <v-app-bar>
+    <v-container class="pa-0">
+      <div class="px-4">
+        <v-row class="ma-0">
+          <v-col class="pa-0" cols="2">
+            <v-img :src="Logo" max-height="46"></v-img>
+          </v-col>
+          <v-col class="pa-0" cols="8" align="center" align-self="center">
+            <span style="font-size: 30px">
+              <strong>Vue3 Prototype</strong>
+            </span>
+          </v-col>
+          <v-col class="pa-0" cols="2" align-self="center">
+            <v-icon>mdi-account-circle</v-icon>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<style scoped></style>
